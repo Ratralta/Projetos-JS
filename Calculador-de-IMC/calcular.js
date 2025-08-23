e4c4942fc947fe23a3fff6ca3820dbc484264521
Ratralta <caualima185@gmail.com>
2025-08-22 21:00:07 -0300

    agora o calculador de imc da sua situação

--- Calculador-de-IMC/calcular.js
+++ Calculador-de-IMC/calcular.js
@@ -13,10 +13,48 @@ function calc_imc()
     let resultado = peso/(altura*altura); // "let" é um tipo de variavel LOCAL 
     resultado = resultado.toFixed(2); // tirando numeros depois da virgula
 
-    resultado_texto.textContent = "Ola " + `${nome}` + ", seu IMC deu : " + `${resultado}`;  
+    let situacao = check_imc_categoria(resultado) // situação da pessoa
+
+    resultado_texto.textContent = "Ola " + `${nome}` + ", seu IMC deu : " + `${resultado}` + ", Sua situação é : " + `${situacao}`;  
+}
+
+function check_imc_categoria(_value)
+{
+    let retorno = "" // variavel que vai retorna
+
+    if (_value < 18.5)
+    {
+    retorno = "Abaixo do Peso"
+    }
+    else if (_value <= 24.9)
+    {
+    retorno = "Normal"
+    }
+    else if (_value <= 29.9)
+    {
+    retorno = "Acima do Peso"
+    }
+    else if (_value <= 34.9)
+    {
+    retorno = "Obeso 1"
+    }
+    else if (_value <= 39.9)
+    {
+    retorno = "Obeso 2"
+    }
+    else if (_value > 39.9)
+    {
+    retorno = "Obeso Ultra"
+    }
+    else // isso não é pra acontecer
+    {
+    retorno = "Você não existe"
+    }
+    return retorno // retornando string depedendo da situação 
 }
 
 
 
 
-calcular.addEventListener('click',calc_imc)
+
+calcular.addEventListener('click',calc_imc) // adicionando um evento para o botão
