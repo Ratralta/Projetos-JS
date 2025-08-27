f4fd1bca4f1dd0cd925a3eac614d81256f4adf11
Ratralta <caualima185@gmail.com>
2025-08-27 13:53:03 +0000

    teste

--- Lampada - Copia/lampada.js
+++ Lampada - Copia/lampada.js
@@ -1,47 +1,30 @@
-const desligar = document.getElementById('botao_desligar'); // o "document" procura o arquivo 
-const image = document.getElementById('lamp'); // seu parametro se baseia em algum ID de alguma tag.
-var quebrada = 0
+const lamp = document.getElementById('lamp');
+const botaoLigar = document.getElementById('botao_ligar');
+const botaoDesligar = document.getElementById('botao_desligar');
+const botaoQuebrar = document.getElementById('botao_quebrar');
+let quebrada = false;
 
-function ligar_lampada()
-{
-    if (check_quebrou_lampada() === 1)
-    {
-    lamp.src = './img/ligada.jpeg' // mudando o caminho da imagem para "ligada.jpeg"
+function ligar_lampada() {
+    if (!quebrada) {
+        lamp.src = './img/ligada.jpeg';
     }
 }
 
-function desligar_lampada()
-{
-    if (check_quebrou_lampada() === 1)
-    {
-    lamp.src = './img/desligada.jpeg' // mudando o caminho da imagem para "ligada.jpeg"
+function desligar_lampada() {
+    if (!quebrada) {
+        lamp.src = './img/desligada.jpeg';
     }
 }
 
-function quebrar_lampada()
-{
-lamp.src = './img/quebrada.jpeg' // mudando o caminho da imagem para "ligada.jpeg"
-quebrada = 1
+function quebrar_lampada() {
+    lamp.src = './img/quebrada.jpeg';
+    quebrada = true;
 }
 
-function check_quebrou_lampada() // retorna 0 ou 1, baseado no valor de "quebrado" 
-{
-    if ( quebrada === 0)
-    {
-    return 1
-    }
-    else
-    {
-    return 0
-    }
-}
+lamp.addEventListener('mouseover', ligar_lampada);
+lamp.addEventListener('mouseleave', desligar_lampada);
+lamp.addEventListener('dblclick', quebrar_lampada);
 
-
-
-
-
-
-lamp.addEventListener('mouseover',ligar_lampada); // liga a lampada
-lamp.addEventListener('dblclick',quebrar_lampada);
-image.addEventListener('mouseleave',desligar_lampada);
-
+botaoLigar.addEventListener('click', ligar_lampada);
+botaoDesligar.addEventListener('click', desligar_lampada);
+botaoQuebrar.addEventListener('click', quebrar_lampada);
