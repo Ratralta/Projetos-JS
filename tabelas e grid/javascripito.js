1fd3afa57e8b74a2dffb1aca568ec8484da502ae
Ratralta <caualima185@gmail.com>
2025-09-09 20:45:55 -0300

    tem um novo ensiando a usar grid e tabela

---
+++ tabelas e grid/javascripito.js
@@ -0,0 +1,74 @@
+function vogais() {
+document.getElementById("quadro1").innerHTML = "A";
+var obj1 = document.getElementById("quadro1");
+obj1.className = "azul";
+
+document.getElementById("quadro2").innerHTML = "B";
+var obj1 = document.getElementById("quadro2");
+obj1.className = "vermelho";
+
+document.getElementById("quadro3").innerHTML = "C";
+var obj1 = document.getElementById("quadro3");
+obj1.className = "verde";
+
+document.getElementById("quadro4").innerHTML = "D";
+var obj1 = document.getElementById("quadro4");
+obj1.className = "amarelo";
+}
+
+
+function romanos() {
+document.getElementById("quadro1").innerHTML = "I";
+var obj1 = document.getElementById("quadro1");
+obj1.className = "azul";
+
+document.getElementById("quadro2").innerHTML = "II";
+var obj1 = document.getElementById("quadro2");
+obj1.className = "vermelho";
+
+document.getElementById("quadro3").innerHTML = "III";
+var obj1 = document.getElementById("quadro3");
+obj1.className = "verde";
+
+document.getElementById("quadro4").innerHTML = "IV";
+var obj1 = document.getElementById("quadro4");
+obj1.className = "amarelo";
+}
+// #################################################################
+
+function grid_vogais() {
+document.getElementById("grid1").innerHTML = "A";
+var obj1 = document.getElementById("grid1");
+obj1.className = "azul";
+
+document.getElementById("grid2").innerHTML = "B";
+var obj1 = document.getElementById("grid2");
+obj1.className = "vermelho";
+
+document.getElementById("grid3").innerHTML = "C";
+var obj1 = document.getElementById("grid3");
+obj1.className = "verde";
+
+document.getElementById("grid4").innerHTML = "D";
+var obj1 = document.getElementById("grid4");
+obj1.className = "amarelo";
+}
+
+
+function grid_romanos() {
+document.getElementById("grid1").innerHTML = "I";
+var obj1 = document.getElementById("grid1");
+obj1.className = "azul";
+
+document.getElementById("grid2").innerHTML = "II";
+var obj1 = document.getElementById("grid2");
+obj1.className = "vermelho";
+
+document.getElementById("grid3").innerHTML = "III";
+var obj1 = document.getElementById("grid3");
+obj1.className = "verde";
+
+document.getElementById("grid4").innerHTML = "IV";
+var obj1 = document.getElementById("grid4");
+obj1.className = "amarelo";
+}
