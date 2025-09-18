d9940a0deccac077b664bdeb9b580294f3a65a55
Ratralta <caualima185@gmail.com>
2025-09-18 20:59:26 -0300

    melhorei o convertedor de temperatura E fiz um coisa de lista

--- Projeto Feitos por mim/calcular temperatura/calcular_temperatura.js
+++ Projeto Feitos por mim/calcular temperatura/calcular_temperatura.js
@@ -1,16 +1,82 @@
+function conveter_calculador(_input_valor,_input_temp_tipo,_converter_tipo) // talvez se eu tivese usado array, teria sido mais facil.
+{
+    let retorno;
+    if(_input_temp_tipo == _converter_tipo) // se tiver o mesmo tipo de grau
+    {
+        retorno = _input_valor;
+    }
+
+    if (_input_temp_tipo == "celsius" && _converter_tipo == "fahrenheit") // celsius para fahrenheit.
+    {
+        retorno = (9/5 * _input_valor) + 32;
+    }
+    if (_input_temp_tipo == "celsius" && _converter_tipo == "kelvin") // celsius para kelvin .
+    {
+        retorno = _input_valor + 273.15;
+    }
+
+    
+    if (_input_temp_tipo == "fahrenheit" && _converter_tipo == "celsius") // fahrenheit para celsius.
+    {
+        retorno = (9/5 * _input_valor) + 32;
+    }
+    if (_input_temp_tipo == "fahrenheit" && _converter_tipo == "kelvin") // fahrenheit para kelvin .
+    {
+        retorno = 5/9 * (_input_valor - 32);
+        retorno = retorno + 273.15;
+    }
+
+
+    if (_input_temp_tipo == "kelvin" && _converter_tipo == "celsius") // kelvin para celsius.
+    {
+        retorno = _input_valor + 273.15;
+    }
+    if (_input_temp_tipo == "kelvin" && _converter_tipo == "fahrenheit") // kelvin para fahrenheit .
+    {
+        retorno = 5/9 * (_input_valor - 32);
+        retorno = retorno + 273.15;
+    }
+    return retorno;
+}
+
+function converter_amostrar_o_tipo_bonito(_converter_tipo)
+{
+    if (_converter_tipo == "celsius")
+    {
+        _converter_tipo = "ºC"
+    }
+    else if (_converter_tipo == "fahrenheit")
+    {
+        _converter_tipo = "ºF"
+    }
+    else if (_converter_tipo == "kelvin")
+    {
+        _converter_tipo = "K"
+    }
+    return _converter_tipo
+}
+
+
+
 function conveter()
 {
-    let input_valor = document.getElementById("input_temp_valor").value;
-    let input_temp_tipo = document.getElementById("input_temp_tipo").value;
+    let input_valor = document.getElementById("input_temp_valor").value;    // valor do usario 
+    let input_temp_tipo = document.getElementById("input_temp_tipo").value; // tipo de temperatura do usuario
+    let converter_tipo = document.getElementById("temperatura_tipo").value; // converter para  
 
-    let converter_tipo = document.getElementById("temperatura_tipo").value;
+    let retorno_tag = document.getElementById("retorno");
+    let retorno_tag_2 = document.getElementById("retorno_tipo"); // eu deveria me basear usando "class" pra fazer isso.
+    let retorno;
 
-    if (input_valor !== "")
+    input_valor = parseInt(input_valor.replace(/[^0-9.]/g,"")); // limpando o valor e transformando em numero 
+    if (input_valor == "" || isNaN(input_valor))
     {
-        console.log("o valor é " + input_valor)
+        alert("Coloca algo animal");
+        return; // encerra função 
     }
-    else
-    {
-        alert("Coloca algo animal")
-    }
+
+    retorno = conveter_calculador(input_valor,input_temp_tipo,converter_tipo); // função para retornar o valor 
+
+    retorno_tag.innerHTML = retorno; // inserindo na tag o resultado 
+    retorno_tag_2.innerHTML = converter_amostrar_o_tipo_bonito(converter_tipo); // inserindo na tag o resultado 
 }
--- ensinando array/Array 2/vetolizador.js
+++ ensinando array/Array 2/vetolizador.js
@@ -4,6 +4,9 @@ let meuVetor = new Array(8);
 let result = ""; // variavel pra juntar dados e colocar no html
 let somaVetor = 0;
 let multVetor = 0;
+let mediaVetor = 0;
+let minVetor;
+let maxVetor;
 let resultTabela = "";
 
 for(let i=0;i<meuVetor.length;i++)
@@ -22,12 +25,32 @@ for(i=0;i<meuVetor.length;i++) // colocando o conteudo de "meuVetor" como uma st
 
     resultTabela += "<td>" + i_vetor + "</td>";
 
+    if (isNaN(minVetor)){ // se o valor de "minVetor" tiver vazio 
+    minVetor = i_vetor;
+    }
+    else{
+    minVetor = Math.min(i_vetor,minVetor)
+    }
+    
+    if (isNaN(maxVetor)){ // se o valor de "maxVetor" tiver vazio 
+    maxVetor = i_vetor;
+    }
+    else{
+    maxVetor = Math.max(i_vetor,maxVetor)
+    }
 }
 multVetor = meuVetor[0] + " X " + meuVetor[meuVetor.length-1] + " = " + meuVetor[0] * meuVetor[meuVetor.length-1];
-
+mediaVetor = somaVetor/meuVetor.length
 
 document.getElementById("resultado").innerHTML = result;
 document.getElementById("soma").innerHTML = somaVetor;
 document.getElementById("produto").innerHTML = multVetor;
+
+document.getElementById("media").innerHTML = mediaVetor;
+document.getElementById("menor").innerHTML = minVetor;
+document.getElementById("maior").innerHTML = maxVetor;
+
+
+
 document.getElementById("tabela").innerHTML = "<table border=3 align=center> <tr>" + resultTabela + "</tr> </table>";
 }
