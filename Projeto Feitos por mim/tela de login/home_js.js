83776203dce0ad8e7a642f6c39fc7e5435aa0152
Ratralta <caualima185@gmail.com>
2025-11-13 20:43:04 -0300

    fazendo javascript da homepage, problema em pegar valores do formulario.

---
+++ Projeto Feitos por mim/tela de login/home_js.js
@@ -0,0 +1,33 @@
+// class
+let User_login = class User_login 
+{
+    constructor(_name,_passowrd)
+    {
+        this.name = _name;
+        this.passowrd = _passowrd;
+    }
+};
+/////// class
+
+
+// funções
+function inserir_result(_tag,_string_result)
+{
+    _tag.innerHTML = _string_result
+}
+/////// funções 
+
+// variaveis
+
+
+// NÃO TA DANDO PRA PEGAR OS VALORES DO FORM ATRAVÉS DO "DOCUMENT"
+let user_nome = document.getElementById(nome).value; 
+let user_senha = document.getElementById(senha).value; 
+let Object_user_dados = new User_login(user_nome,user_senha) // objeto com os dados enviados no "submit" do "login"
+
+let string_result = user_nome + "<br>" + user_senha // texto que será colocado na tag
+let html_tag_result = document.getElementById(tag_result); // tag que ira inserir o resultado 
+////// variaveis
+
+
+inserir_result(html_tag_result,string_result);
