f9e06dafe72da200f55831c74b7f70e4cc7fe729
Ratralta <caualima185@gmail.com>
2025-11-18 20:53:08 -0300

    agora "home.html" consegue pegar os dados do form

--- Projeto Feitos por mim/tela de login/home_js.js
+++ Projeto Feitos por mim/tela de login/home_js.js
@@ -21,13 +21,15 @@ function inserir_result(_tag,_string_result)
 
 
 // NÃO TA DANDO PRA PEGAR OS VALORES DO FORM ATRAVÉS DO "DOCUMENT"
-let user_nome = document.getElementById(nome).value; 
-let user_senha = document.getElementById(senha).value; 
+const form = new URLSearchParams(window.location.search) // cria um objeto do form recebido. (GET)
+
+let user_nome = form.get("nome")
+let user_senha = form.get("senha")
 let Object_user_dados = new User_login(user_nome,user_senha) // objeto com os dados enviados no "submit" do "login"
 
 let string_result = user_nome + "<br>" + user_senha // texto que será colocado na tag
-let html_tag_result = document.getElementById(tag_result); // tag que ira inserir o resultado 
+let html_tag_result = document.getElementById("tag_result"); // tag que ira inserir o resultado 
 ////// variaveis
 
 
-inserir_result(html_tag_result,string_result);
+inserir_result(html_tag_result,string_result)
